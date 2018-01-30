93f0c71a67517871b498731c7fef56ac8c772e1d
1hea <727643332@qq.vom>
2018-01-30 17:39:20 +0800

    不提前换行的TextView
    一个不提前换行的TextView

--- FillTextView.java
+++ FillTextView.java
@@ -1,25 +1,30 @@
-package two.isay.com.simpletext.widget;
+package two.isay.com.simpletext.text;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
+import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
 
 /**
- * Created by cywu4 on 2017/10/27.
- * <p>
- * 解决TextView包含英文数字提前换行问题（有较强的针对性）
+ * 不自动提前换行的TextView
+ * Created by cywu4 on 2018/1/26.
  */
 
 public class FillTextView extends TextView {
 
-    private int mLayoutWidth = 0;
-    private static int screenWidth = 0;
+    //文字显示的宽度
+    private int mWidthBoring;
+    //原文字
+    private String mText;
 
     public FillTextView(Context context) {
         super(context);
@@ -27,55 +32,89 @@ public class FillTextView extends TextView {
 
     public FillTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        if (screenWidth == 0) {
-            WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-            DisplayMetrics dm = new DisplayMetrics();
-            wm.getDefaultDisplay().getMetrics(dm);
-            screenWidth = dm.widthPixels;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.AT_MOST) {
+            mWidthBoring = MeasureSpec.getSize(widthMeasureSpec);
+        } else {
+            mWidthBoring = getWidth() - getPaddingLeft() - getPaddingRight();
         }
     }
 
 
-    public void setFillText(CharSequence text) {
-        Paint paint = getPaint();
-        if (TextUtils.isEmpty(text) || paint == null || getLayoutWidth() == 0) {
-            setText(text);
+    /**
+     * 获取文字内容
+     *
+     * @return 原文字
+     */
+    @Override
+    public CharSequence getText() {
+        return mText;
+    }
+
+
+    /**
+     * 设置文字
+     *
+     * @param text 显示的文字
+     */
+    public void setText(String text) {
+        mText = text;
+        int length = text == null ? 0 : text.length();
+        if (length <= 1) {
+            setText(text, BufferType.NORMAL);
             return;
         }
-        String str = text.toString();
-        String s1 = "";
-        String s2 = "";
-        if (paint.measureText(str) > mLayoutWidth) {
-            for (int i = 15, length = str.length(); i < length; i++) {
-                if (i <= length) {
-                    if (paint.measureText(str.substring(0, i)) > mLayoutWidth) {
-                        s1 = str.substring(0, i - 1);
-                        s2 = str.replace(s1, "");
-                        break;
+        TextPaint paint = getPaint();
+        //估算每行文字个数
+        int charWidth = (int) paint.measureText(text.substring(0, 1));
+        if (charWidth + 1 > mWidthBoring) {
+            setText(text, BufferType.NORMAL);
+            return;
+        }
+        int rowCount = mWidthBoring / charWidth;
+        //切割内容文字
+        StringBuilder builder = new StringBuilder();
+        int start = 0;
+        int end = start + rowCount;
+        while (true) {
+            //不能超出最大长度
+            if (end > length) {
+                end = length;
+            }
+            //测量start~end字符的长度
+            if (paint.measureText(text.substring(start, end)) > mWidthBoring) {
+                while (paint.measureText(text.substring(start, --end)) > mWidthBoring) {
+                    continue;
+                }
+            } else {
+                if (end == length) {
+                    builder.append(text.substring(start, end));//结束所有循环
+                    break;
+                }
+                while (paint.measureText(text.substring(start, ++end)) < mWidthBoring) {
+                    if (end == length) {
+                        ++end;
+                        break;//结束本次内循环
                     }
                 }
+                --end;//超出宽度前的一个
             }
-        }
-        if (!TextUtils.isEmpty(s1)) {
-            if (!TextUtils.isEmpty(s2)) {
-                str = s1 + "\n" + s2;
-            } else {
-                str = s1;
+            builder.append(text.substring(start, end));
+            if (end < length) {
+                builder.append("\n");
+            } else if (end >= length) {
+                break;
             }
+            //重置起始位
+            rowCount = end - start;
+            start = end;
+            end = start + rowCount;
         }
-        setText(str);
+        setText(builder.toString(), BufferType.NORMAL);
     }
-
-
-    private int getLayoutWidth() {
-        if (mLayoutWidth == 0) {
-            mLayoutWidth = screenWidth - getPaddingLeft() - getPaddingRight();
-        }
-        if (mLayoutWidth < 0) {
-            mLayoutWidth = 0;
-        }
-        return mLayoutWidth;
-    }
-
 
 }
